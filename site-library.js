86ae45d650595d8e6bb803dbcfd3cacbf234e7aa
Thomas Ballard <thomas.ballard@gmail.com>
2021-06-10 03:02:43 -0700

    fiddling with font, dark/light mode, and some state

---
+++ site-library.js
@@ -0,0 +1,41 @@
+(function () {
+    // TODO: convert to module?
+
+    window.addEventListener('DOMContentLoaded', (event) => {
+        const hour = new Date().getHours();
+        const mode = localStorage && localStorage.getItem('mode') 
+            || (hour > 6 && hour < 17 ? '' : 'dark') 
+            || ''
+        document.querySelector('body').classList.toggle(mode);
+
+        addModeButton(mode);
+
+    });
+
+    addModeButton = (mode) => {
+        let el = document.createElement('button');
+        el.innerHTML = `Mode: ${mode}`;
+        el.style.position = 'fixed';
+        el.style.top = '1em';
+        el.style.right = '1em';
+
+        el.onclick = (ev) => {
+            // toggle the theme manually
+            // update the button text
+            // store the user preference
+
+            let body = document.querySelector('body');
+                body.classList.toggle('dark');
+            let darkMode = body.classList.contains('dark');
+            let mode = darkMode ? 'dark' : 'light';
+
+            ev.target.innerHTML = `Mode: ${mode}`;
+
+            localStorage.setItem('mode', mode);
+        }
+
+        document.body.appendChild(el);
+    }
+
+
+})()
